572755f5fb5d45a404e449e425767063c8f331be
Mathis <handymathis@gmail.com>
2021-03-17 18:44:45 +0100

    added Header.js

---
+++ web/components/Header.js
@@ -0,0 +1,16 @@
+import styles from '../styles/Header.module.css';
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+export default function Header() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.heading}>SLF-engine</div>
+            <div className={styles.linkBox}>
+                <a href={"https://github.com/MathisBurger/SLF-engine"}>
+                    <FontAwesomeIcon icon={faGithub} size={"5x"} />
+                </a>
+            </div>
+        </div>
+    );
+}
