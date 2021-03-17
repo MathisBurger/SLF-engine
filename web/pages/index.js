d881d19ebbed4b92b89e441d8df259d8222831e9
Mathis <handymathis@gmail.com>
2021-03-17 19:20:07 +0100

    added search input

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,13 +1,14 @@
 import Header from '../components/Header';
-import {faGithub} from "@fortawesome/free-brands-svg-icons";
-import styles from "../styles/Header.module.css";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import style from '../styles/Home.module.css';
+
 
 export default function Home() {
   return (
     <div>
         <Header />
-      <p>a</p>
+      <div className={style.container}>
+          <input className={style.searchbar} placeholder={"city with a"} />
+      </div>
     </div>
   )
 }
